540eb46af05744c32e7be2cfb82d97e357824490
Charlie Choiniere <nek4life@gmail.com>
2011-07-18 21:44:38 -0400

    Updated temp folder settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 mv $HOME/.vim $HOME/.vimbak
 mv $HOME/.vimrc $HOME/.vimrcbak
 mv $HOME/.gvimrc $HOME/.gvimrcbak
-ln -s `pwd` $HOME/.vim
-ln -s `pwd`/vimrc $HOME/.vimrc
-ln -s `pwd`/gvimrc $HOME/.gvimrc
+ln -nfs `pwd` $HOME/.vim
+ln -nfs `pwd`/vimrc $HOME/.vimrc
+ln -nfs `pwd`/gvimrc $HOME/.gvimrc
